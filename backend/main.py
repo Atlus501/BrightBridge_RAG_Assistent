6f937cafefe9a5a8d5d4d1ac6d9e733e2ba0efb0
Atlus501 <helloworld4846@gmail.com>
2026-07-09 20:59:05 -0500

    testing protocol changes

--- backend/main.py
+++ backend/main.py
@@ -1,30 +1,22 @@
 from fastapi import FastAPI, Response, Request, status
 from fastapi.middleware.cors import CORSMiddleware
-from contexlib improt asynccontextmanager
+from contextlib import asynccontextmanager
 from pydantic import ValidationError
 import logging
 import uvicorn
 import os
 
-from api.schema.request_body import RAG_Request_Body, Context_Request_Body
+from api.schemas.request_body import RAG_Request_Body, Context_Request_Body
 from services.helper_functions import load_env, setup_rag, set_up_context_manager, get_rag_response
 
 allowed_origins = [
     "http://localhost:8000", # If you also test locally
 ]
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=allowed_origins,           # Allowed domains list
-    allow_credentials=True,          # Permit cookies / auth headers
-    allow_methods=["*"],             # Allow all HTTP methods (GET, POST, etc.)
-    allow_headers=["*"],             # Allow all custom HTTP headers
-)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # [Startup]: Triggered before the server starts accepting requests
     logging.basicConfig(level=logging.WARNING, filename="brightbridge.log", format='%(asctime)s - %(levelname)s - %(message)s')
-    load_env()
     app.state.rag = setup_rag()
     app.state.context_manager = set_up_context_manager()
 
@@ -39,6 +31,14 @@ app = FastAPI(
     lifespan = lifespan,
     description="Asynchronous backend service managing text context vector lookups",
     version="1.0.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_origins,           # Allowed domains list
+    allow_credentials=True,          # Permit cookies / auth headers
+    allow_methods=["*"],             # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],             # Allow all custom HTTP headers
 )
 
 #api used for responding to requests
@@ -128,6 +128,7 @@ async def get_past_context(request_body : Context_Request_Body, request : Reques
 
 #the code that will trigger if is it is set to be the main code
 if __name__ == "__main__":
+    load_env()
     SERVER_IP = os.getenv("SERVER_IP")
     SERVER_PORT = int(os.getenv("SERVER_PORT"))
     uvicorn.run("main:app", host=SERVER_IP, port=SERVER_PORT, reload=True)
---
+++ backend/tests/network_test.py
@@ -0,0 +1,41 @@
+import requests
+
+endpoints = ["/respond", "/store_past_context", "/get_past_context"]
+payloads = [
+    {
+        "past_conv": [],
+        "prompt" : "What are some good relationship therapy options?"
+    },
+
+    {
+        "session_id" : None,
+        "password": "soidfjskfdkwortiwurtxkvj",
+        "actor_id": "test-user",
+        "conv_to_be_saved": [],
+    }
+]
+
+def test_server_endpoint(api_endpoint, payload):
+  try:
+    host_ip = "127.0.0.1"
+    host_port = 8080
+    r = requests.post(f"http://{host_ip}:{host_port}{api_endpoint}", json=payload)
+    print(r.json())
+    return r
+  except Exception as e:
+    print(e)
+    return {"error" : str(e)}
+
+#the part that tests the response endpoint
+r = test_server_endpoint(endpoints[0], payloads[0])
+
+#adjusting the nodes to save the response
+payloads[1]['conv_to_be_saved'] = ["User: What are some good relationship therapy options?\n Assistant: Based on the context provided, here are some good relationship therapy options:  *   **Emotionally-Focused-Couple-\n"]
+
+r = test_server_endpoint(endpoints[1], payloads[1])
+
+#adjusting the nodes to retrieve the resposne
+payloads[1]['session_id'] = r.json()['session_id']
+
+r = test_server_endpoint(endpoints[2], payloads[1])
+
